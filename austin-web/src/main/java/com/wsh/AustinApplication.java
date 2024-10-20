1d65c432d17012955e99c82e9ef334041baf287b
Shouheng-Wang <2097668797@qq.com>
2024-10-20 18:09:29 +0800

    git stash

--- austin-web/src/main/java/com/wsh/AustinApplication.java
+++ austin-web/src/main/java/com/wsh/AustinApplication.java
@@ -1,29 +1,39 @@
 package com.wsh;
 
+import com.wsh.pojo.SmsParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 @SpringBootApplication
 @RestController
 public class AustinApplication {
 
-    private final Logger logger = LoggerFactory.getLogger(AustinApplication.class);
-
+    @Autowired
+    private TencentSmsScript tencentSmsScript;
     public static void main(String[] args) {
         SpringApplication.run(AustinApplication.class, args);
     }
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        logger.error("error logback for austin");
-        logger.info("info logback for austin");
-        return String.format("Hello %s!", name);
+    public String hello() {
+
+        SmsParam smsParam = SmsParam.builder()
+                .phones(new HashSet<>(Arrays.asList("13719193845")))
+                .content("3333")
+                .build();
+
+        return tencentSmsScript.send(smsParam);
+
+
     }
 
 }
-
